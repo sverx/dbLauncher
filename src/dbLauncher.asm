052f0daa7cd19ad3eb2664ee927ce9c23dd7b6c5
sverx <sverx@yahoo.com>
2014-11-07 17:22:13 +0100

    graphical menu, with tentative images
    (the output.sms ROM expects 4 128KB ROMs to be appended to it...)

--- src/dbLauncher.asm
+++ src/dbLauncher.asm
@@ -18,26 +18,52 @@ banks 1
 .bank 0 slot 0
 .org $0000                      ; this goes at ROM address 0 (boot)
 .section "Startup" force
-  ; (no mapper/interrupt mode initializazion needed here!)
   di                            ; disable interrupt
-  ld sp,$dff0                   ; set stack pointer at end of RAM
+  im 1                          ; interrupt mode 1 (this won't change)
+  ld sp, $dff0                  ; set stack pointer at end of RAM
   jp main                       ; run main
+                                ; (no mapper mode initializazion needed here!)
 .ends
 
 .org $0038
 .section "Interrupt handler" force
-  reti                          ; return from interrupt (stub)
+  push af
+    in a,(VDPStatusPort)        ; read port to satisfy interrupt
+    ld a,$01                    ; the only interrupt enabled is VBlank so...
+    ld (VBlankFlag),a           ;   ... write down that it actually happened
+  pop af
+  ei                            ; enable interrupt (that were disabled by the IRQ call)
+  reti                          ; return from interrupt
 .ends
 
 .org $0066
-.section "Pause handler" force
+.section "Pause handler" force  ; no PAUSE support in the launcher
   retn                          ; return from NMI (stub)
 .ends
 
-.SDSCTAG 0.01, "dbLauncher","Boot ROM to launch homebrew ROMs in db-elec's SMS cartridge","2014/SVERX"
-; since 2014-10-16 it's working properly even with 16KB ROMs,
+.SDSCTAG 0.11, "dbLauncher","Boot ROM to launch homebrew ROMs in db-elec's SMS cartridge","2014/SVERX"
+; since 2014-10-16 WLA-DX it's working properly even with 16KB ROMs,
 ; adding SDSC header (homebrew) and SEGA ROM header ("TMR SEGA"
 ; signature, ROM checksum and correct ROMtype/ROMsize byte)
+
+.include "base.inc"
+.include "vdp.inc"
+.include "PSGaiden_tile_decomp.inc"
+
+; ******** ASSETS ********
+.section "assets" free
+multicart_tiles:
+.incbin "inc/multicart (tiles).psgcompr"
+multicart_tilemap:
+.incbin "inc/multicart (tilemap).bin"
+multicart_palette:
+.incbin "inc/multicart (palette).bin"
+arrow_tiles:
+.incbin "inc/arrow (tiles).psgcompr"
+arrow_palette:
+.incbin "inc/arrow (palette).bin"
+.ends
+
 
 .section "bankshift table" free
 ; table where bankshift values for each game are stored
@@ -48,6 +74,30 @@ bankshift_table:
   .db  1,9,17,25
 .ends
 
+.section "turnOnVideo" free
+turnOnVideo:
+  ; enable sprites 8x16, turn on video
+  SETVDPREGSANDRET VDPReg1_Mode|Reg1_BigSprites|Reg1_Show|Reg1_FrameInt
+.ends
+
+.section "turnOffVideo" free
+turnOffVideo:
+  ; disable sprites 8x16, turn off video
+  SETVDPREGSANDRET VDPReg1_Mode|$00
+.ends
+
+.section "waitForVBlank" free
+waitForVBlank:
+  xor a
+  ld (VBlankFlag),a     ; reset VBlank - we are waiting for the next one
+-:ld a,(VBlankFlag)
+  or a
+  jr z,-
+  xor a
+  ld (VBlankFlag),a
+  ret
+.ends
+
 .section "main" free
 main:
   ; copy the launcher code to RAM
@@ -55,29 +105,110 @@ main:
   ld de,launcher_RAM
   ld bc,launcher_end-launcher
   ldir
+  
+  ; set VDP to default Mode4 values
+  call SetMode4Default
+  
+  ; load assets (tiles/tilemap/palette) in VRAM
+  ld hl,multicart_tiles
+  ld de,$0000|$4000              ; from VRAM $0000
+  call PSGaiden_tile_decompr
 
+  ld hl,arrow_tiles
+  ld de,$2000|$4000              ; from VRAM $2000 (sprites)
+  call PSGaiden_tile_decompr
+
+  SetVDPAddress $3800|$4000
+  ld hl,multicart_tilemap
+  ld c,VDPDataPort
+  ld b,0                         ; 0 = 256 bytes
+  .rept 6                        ; 6 times 256 bytes = 32*24*2 = whole map
+    otir
+  .endr
+  
+  ld hl,arrow_palette
+  call WriteSpritePalette
+
+  ld hl,multicart_palette
+  call WriteSpritePalette_zerocolor
+
+  ld hl,multicart_palette
+  call WriteBGPalette
+  
+  ; set sprite info
+  SetVDPAddress $3F00|$4000
+  ld c,VDPDataPort
+  ld a,110                 ; sprite Y
+  out (c),a
+  out (c),a
+  ld a,$D0                 ; no more sprites
+  out (c),a
+
+  SetVDPAddress $3F00+128|$4000
+  ld c,VDPDataPort
+  ld a,128-8               ; sprite X
+  out (c),a
+  ld a,0                   ; tile 0
+  out (c),a
+  ld a,128-8+8             ; sprite X
+  out (c),a
+  ld a,2                   ; tile 0
+  out (c),a
+
+  call turnOnVideo
+  ei
+  
+  xor a
+  ld (WhichGame),a         ; selected game = 0
+  
 loop:
+  call waitForVBlank       ; wait until VBlank starts
+
+  ; update sprite
+  SetVDPAddress $3F00+128+1|$4000
+  ld c,VDPDataPort
+  ld a,(WhichGame)
+  ASLA 2
+  out (c),a
+
+  SetVDPAddress $3F00+128+3|$4000
+  ld c,VDPDataPort
+  ld a,(WhichGame)
+  ASLA 2
+  inc a
+  inc a
+  out (c),a
+
   in a,($dc)           ; read pad
   cpl                  ; A = ~A
 
   cp $01               ; UP=first game selected
   jr nz,+
-  ld a,(bankshift_table)
-  jp launcher_RAM
+  ld a,0
+  ld (WhichGame),a
 
 +:cp $02               ; DOWN=second game selected
   jr nz,+
-  ld a,(bankshift_table+1)
-  jp launcher_RAM
+  ld a,1
+  ld (WhichGame),a
 
 +:cp $04               ; LEFT=third game selected
   jr nz,+
-  ld a,(bankshift_table+2)
-  jp launcher_RAM
+  ld a,2
+  ld (WhichGame),a
 
 +:cp $08               ; RIGHT=fourth game selected
   jr nz,+
-  ld a,(bankshift_table+3)
+  ld a,3
+  ld (WhichGame),a
+  
++:cp $10               ; START!
+  jr nz,+
+  call turnOffVideo
+  ld a,(WhichGame)
+  ld hl,bankshift_table
+  ADDHLA
+  ld a,(hl)
   jp launcher_RAM
 
 +:jp loop
@@ -109,8 +240,8 @@ launcher_end:
 .ends
 
 .ramsection "RAM" slot 1
-  ; launcher_RAM         dsb launcher_end-launcher   ;  here's where the lancher copy goes
-  ;                      can't make this here work :|
-  launcher_RAM         dsb 256 ;  here's where the lancher copy goes
-  ;                      256 bytes are surely enough :)
+  VBlankFlag      db      ;  the flag for VBlank
+  WhichGame       db      ;  which game
+  launcher_RAM    dsb 256 ;  here's where the launcher copy goes
+                          ;  256 bytes are surely enough :)
 .ends
